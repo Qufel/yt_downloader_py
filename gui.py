396fed58d99bd7d8ff5750a50fdc5af778a1af1c
Qufel <97023524+Qufel@users.noreply.github.com>
2022-09-18 21:14:04 +0200

    changed gui.py and main.py, deleted func.py

--- gui.py
+++ gui.py
@@ -1,86 +1,140 @@
+from faulthandler import disable
+from os import stat
 from tkinter import *
 from tkinter import ttk
-import func
+from turtle import color, width
 
 #window size
-sizeX = 800
+sizeX = 1000
 sizeY = 600
+
+global root
+#declaring root
+root = Tk()
+root.geometry(f'{sizeX}x{sizeY}')
+root.resizable(False,False)
+root.title("yt_downloader_py")
 
 def App():
 
-    #declaring root
-    root = Tk()
-    root.geometry(f'{sizeX}x{sizeY}')
-    root.resizable(False,False)
-    root.title("yt_downloader_py")
+    try:
+        #declaring content frame
+        content = ttk.Frame(root)
+        content['padding'] = 5
+        content.grid(column=0,row=0)
 
-    #declaring content frame
-    content = ttk.Frame(root)
-    content['padding'] = 5
-    content.grid(column=0,row=0)
+        #declaring top frame
+        topF = ttk.Frame(content, relief="solid", width=sizeX - 10, height=sizeY/5)
+        topF.grid_propagate(False)
+        topF.pack_propagate(False)
+        topF['padding'] = 5
+        topF.grid(column=0,row=0,columnspan=3)
 
-    #declaring top frame
-    topF = ttk.Frame(content, relief="solid", width=sizeX - 10, height=sizeY/5)
-    topF.grid_propagate(False)
-    topF.pack_propagate(False)
-    topF['padding'] = 5
-    topF.grid(column=0,row=0,columnspan=3)
+        topFheight = topF.winfo_height()
+        topFwidth = topF.winfo_width()
 
-    topFheight = topF.winfo_height()
-    topFwidth = topF.winfo_width()
+        #Browse Path
 
-    #Browse Path
+        div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
+        div.pack_propagate(False)
+        div.grid(column=0,row=0)
 
-    div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
-    div.pack_propagate(False)
-    div.grid(column=0,row=0)
+        lbl = ttk.Label(div,text='Download Path:').pack(side=LEFT, padx=5)
+        destEntry = ttk.Entry(div, width=int(sizeX))
 
-    lbl = ttk.Label(div,text='Download Path:').pack(side=LEFT, padx=5)
-    destEntry = ttk.Entry(div, width=int(sizeX/8))
-    browse = ttk.Button(div, text='Browse', command=lambda: func.browse_for_dest(destEntry))
-    #Spacing
+        from main import browse_for_dest
+        browse = ttk.Button(div, text='Browse', command=lambda: browse_for_dest(destEntry))
 
-    div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/3)
-    div.pack_propagate(False)
-    div.grid(column=0,row=1)
+        #Spacing | Non-valid-url error lbl
 
-    #Enter URL
+        div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/3)
+        div.pack_propagate(False)
+        div.grid(column=0,row=1)
 
-    div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
-    div.pack_propagate(False)
-    div.grid(column=0,row=2)
+        pathErrorLbl = ttk.Label(div, text="Provided path is invalid!", foreground='red')
 
-    lbl = ttk.Label(div,text='Enter URL:').pack(side=LEFT, padx=5)
-    urlEntry = ttk.Entry(div, width=int(sizeX/6))
-    
-    #ExtensionDD and AddBtn
+        #Enter URL
 
-    div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
-    div.pack_propagate(False)
-    div.grid(column=0,row=3)
+        div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
+        div.pack_propagate(False)
+        div.grid(column=0,row=2)
 
-    dd = ttk.Combobox(div, state='readonly')
-    addBtn = ttk.Button(div,text='Add',command=lambda: func.add_to_list(urlEntry.get(),dd.get()))
+        lbl = ttk.Label(div,text='Enter URL:').pack(side=LEFT, padx=5)
+        urlEntry = ttk.Entry(div, width=int(sizeX/6))
+        
+        #ExtensionDD and AddBtn | Non-valid-url error lbl
 
-    ext=['mp4','webm','mp3','m4a']
-    dd['values'] = ext
-    dd.current(0)
-    
-    
-    #declaring bottom frame
+        div = ttk.Frame(topF,relief="flat", width=sizeX-20, height=sizeY/6/4)
+        div.pack_propagate(False)
+        div.grid(column=0,row=3)
 
-    btmF = ttk.Frame(content, relief="solid", width=sizeX - 10, height=sizeY-(sizeY/5) -15)
-    btmF.grid_propagate(False)
-    btmF['padding'] = 5
-    btmF.grid(column=0,row=1,columnspan=3, pady=5)
+        urlErrorLbl = ttk.Label(div, text="Provided URL is invalid!", foreground='red')
 
-    #packing
-    browse.pack(side=RIGHT)
-    destEntry.pack(side=LEFT)
-    addBtn.pack(side=RIGHT)
-    urlEntry.pack(side=LEFT)
-    dd.pack(side=RIGHT)
+        dd = ttk.Combobox(div, state='readonly')
+
+        from main import add_to_list
+        addBtn = ttk.Button(div,text='Add',command=lambda: add_to_list(urlEntry.get(),dd.get()))
+
+        ext=['mp4','webm','mp3','m4a']
+        dd['values'] = ext
+        dd.current(0)
+        
+        
+        #declaring bottom frame
+
+        btmF = ttk.Frame(content, relief="solid", width=sizeX - 10, height=sizeY-(sizeY/5) -15)
+        btmF.grid_propagate(False)
+        btmF.pack_propagate(False)
+        btmF['padding'] = 5
+        btmF.grid(column=0,row=1,columnspan=3, pady=5)
+
+        #Treeview
+        global videoList
+        from main import treeviewColumns
+
+        listScroll = ttk.Scrollbar(btmF)
+        listScroll.pack(side=RIGHT,fill=Y)
+
+        videoList = ttk.Treeview(btmF, yscrollcommand=listScroll.set)
+        videoList['columns'] = treeviewColumns
+
+        headerSize = int(sizeX/7)
+
+        videoList.column('#0', anchor=W,width=headerSize + 200)
+        videoList.column('#1', anchor=W,width=headerSize)
+        videoList.column('#2', anchor=W,width=headerSize - 20)
+        videoList.column('#3', anchor=W,width=headerSize - 70)
+        videoList.column('#4', anchor=W,width=headerSize - 70)
+        videoList.column('#5', anchor=W,width=headerSize - 70)
+        videoList.column('#6', anchor=W,width=headerSize - 30)
+        videoList.column('#7', anchor=W,width=0, stretch=NO)
+
+        videoList.heading("#0",text='URL', anchor=W)
+        videoList.heading("#1",text='Title', anchor=W)
+        videoList.heading("#2",text='Author', anchor=W)
+        videoList.heading("#3",text='Length', anchor=W)
+        videoList.heading("#4",text='Extension', anchor=W)
+        videoList.heading("#5",text='Progress', anchor=W)
+        videoList.heading("#6",text='Status', anchor=W)
+
+
+
+        #packing
+        browse.pack(side=RIGHT)
+        destEntry.pack(side=LEFT, padx=5)
+        addBtn.pack(side=RIGHT)
+        urlEntry.pack(side=LEFT)
+        dd.pack(side=RIGHT)
+        pathErrorLbl.pack(side=LEFT, padx=5)
+        urlErrorLbl.pack(side=LEFT, padx=5)
+
+        videoList.pack(padx=5,pady=5, fill=Y)
+    except Exception as e:
+        # NULL main window - Throws error after app is closed
+        print(e)
 
     #mainloop
     root.mainloop()
+    
 
+    
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from tkinter import StringVar
+from logging import root
+from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk
 import gui
+
+global isPathValid
+global isUrlValid
+global folderPath
+global treeviewColumns
+
+treeviewColumns = ('URL','Title','Author','Length','Extension','Progress','Status')
+
+def browse_for_dest(entry):
+    print('Browsing for destination...')
+    folderPath = filedialog.askdirectory()
+    entry.delete(0,END)
+    entry.insert(0,folderPath)
+    print(folderPath)
+    return
+
+def add_to_list(url,ext):
+    print('Adding video to list...')
+
+    print(f'{url}.{ext}')
+    return
 
 #runs gui
 gui.App()
+
+
